ce3c6bd8f6497ee6e6dc66ac2f1b2b2b1805a0a4
Dilini Tharaka <127307898+TharinduNimesh@users.noreply.github.com>
2023-11-03 02:19:31 +0530

    order_details_stillNot complete
    I dont know how the appearence is, as i unable to run emulator in my lap

---
+++ app/src/main/java/com/mad/easy_way/OrderDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.mad.easy_way;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
