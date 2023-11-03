701ef96577fadd1257fde3ded28bf4d43cbc3505
Dilini Tharaka <130860514+dilini-tharaka@users.noreply.github.com>
2023-11-04 02:41:18 +0530

    order detail+confirmation layout part 2

--- app/src/main/java/com/mad/easy_way/OrderDetailsActivity.java
+++ app/src/main/java/com/mad/easy_way/OrderDetailsActivity.java
@@ -1,13 +1,28 @@
 package com.mad.easy_way;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderDetailsActivity extends AppCompatActivity {
+
+    EditText edYourEmp,edTaskDetails,edSAddress,edCity,edDistrict,edLocation;
+
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_details);
+        setContentView(R.layout.activity_order_detail);
+
+        edYourEmp = findViewById(R.id.orderYourEmp);
+        edTaskDetails = findViewById(R.id.orderTaskDetails);
+        edSAddress = findViewById(R.id.orderStreetAddress);
+        edCity = findViewById(R.id.orderCity);
+        edDistrict = findViewById(R.id.orderDistrict);
+        edLocation= findViewById(R.id.orderLocation);
+        btn = findViewById(R.id.btnordertopayment);
     }
 }
