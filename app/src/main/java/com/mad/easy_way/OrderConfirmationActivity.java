fb456188e107ccf5076223f6efc3f3e5a652191f
Dilini Tharaka <130860514+dilini-tharaka@users.noreply.github.com>
2023-11-05 02:22:17 +0530

    oder detail+confirm+payment 03

---
+++ app/src/main/java/com/mad/easy_way/OrderConfirmationActivity.java
@@ -0,0 +1,15 @@
+package com.mad.easy_way;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirmation);
+    }
+}
+
--- app/src/main/java/com/mad/easy_way/OrderDetailsActivity.java
+++ app/src/main/java/com/mad/easy_way/OrderDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.mad.easy_way;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,5 +27,24 @@ public class OrderDetailsActivity extends AppCompatActivity {
         edDistrict = findViewById(R.id.orderDistrict);
         edLocation= findViewById(R.id.orderLocation);
         btn = findViewById(R.id.btnordertopayment);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String TaskDetails = edTaskDetails.getText().toString();
+                String SAddress = edSAddress.getText().toString();
+                String City = edCity.getText().toString();
+                String District = edDistrict.getText().toString();
+                String Location = edLocation.getText().toString();
+
+                if (TaskDetails.length() == 0 || SAddress.length() == 0 || City.length() == 0 || District.length() == 0 || Location.length()==0) {
+                    Toast.makeText(getApplicationContext(), "Please fill in all details", Toast.LENGTH_SHORT).show();
+                } else {
+                        startActivity(new Intent(OrderDetailsActivity.this, PayinformationActivity.class));
+                }
+            }
+        });
+
     }
+
 }
